70fc92bb5afb79303972c78ff31fbf8730623c5d
rezaali <syed.reza.ali@gmail.com>
2019-07-22 12:38:21 -0700

    Initial Commit

---
+++ Satin/Satin.h
@@ -0,0 +1,19 @@
+//
+//  Satin.h
+//  Satin
+//
+//  Created by Reza Ali on 7/22/19.
+//  Copyright © 2019 Reza Ali. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Satin.
+FOUNDATION_EXPORT double SatinVersionNumber;
+
+//! Project version string for Satin.
+FOUNDATION_EXPORT const unsigned char SatinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Satin/PublicHeader.h>
+
+
