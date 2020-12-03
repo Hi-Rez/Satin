b8aab4bf904df5ca8941a37bb0f43a4200c0b6fb
rezaali <syed.reza.ali@gmail.com>
2020-12-03 00:29:55 -0800

    added easy way to create a camera ray

--- Source/Math/Ray.swift
+++ Source/Math/Ray.swift
@@ -16,6 +16,11 @@ open class Ray {
         self.direction = simd_make_float3(0.0, 0.0, 1.0)
     }
     
+    public convenience init(_ camera: Camera, _ coordinate: simd_float2) {
+        self.init()
+        setFromCamera(camera, coordinate)
+    }
+    
     public init(_ origin: simd_float3, _ direction: simd_float3) {
         self.origin = origin
         self.direction = direction
@@ -25,20 +30,15 @@ open class Ray {
         return direction * t + origin
     }
     
-    
-    public func setFromCamera(_ camera: Camera, _ coordinate: simd_float2)
-    {
+    public func setFromCamera(_ camera: Camera, _ coordinate: simd_float2) {
         if camera is PerspectiveCamera {
-            self.origin = camera.worldPosition
+            origin = camera.worldPosition
             let unproject = camera.unProject(coordinate)
-            self.direction = normalize(simd_make_float3(unproject.x - origin.x, unproject.y - origin.y, unproject.z - origin.z))
-        }
-        else if camera is OrthographicCamera {
-            self.origin = camera.unProject(coordinate)
-            self.direction = normalize(simd_make_float3(camera.worldMatrix * simd_float4(0.0, 0.0, -1.0, 0.0)))
+            direction = normalize(simd_make_float3(unproject.x - origin.x, unproject.y - origin.y, unproject.z - origin.z))
         }
         else {
-            fatalError("Ray has not implemented this type of Camera")
+            origin = camera.unProject(coordinate)
+            direction = normalize(simd_make_float3(camera.worldMatrix * simd_float4(0.0, 0.0, -1.0, 0.0)))
         }
     }
 }
