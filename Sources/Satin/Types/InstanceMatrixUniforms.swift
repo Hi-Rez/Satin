9d6204c08d421d55f944f0b5cc2026ee3e4628c0
Reza Ali <syed.reza.ali@gmail.com>
2023-04-29 12:29:27 -0700

    updated InstanceMatrixUniforms to have a public constructor

--- Sources/Satin/Types/InstanceMatrixUniforms.swift
+++ Sources/Satin/Types/InstanceMatrixUniforms.swift
@@ -13,7 +13,7 @@ public struct InstanceMatrixUniforms {
     public var modelMatrix: float4x4
     public var normalMatrix: float3x3
 
-    init(modelMatrix: float4x4 = matrix_identity_float4x4, normalMatrix: float3x3 = matrix_identity_float3x3) {
+    public init(modelMatrix: float4x4 = matrix_identity_float4x4, normalMatrix: float3x3 = matrix_identity_float3x3) {
         self.modelMatrix = modelMatrix
         self.normalMatrix = normalMatrix
     }
